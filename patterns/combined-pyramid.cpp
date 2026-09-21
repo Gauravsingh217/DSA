9d9fc66b49f09f7140e0795654d900d709a5fa6c
Gauravsingh217 <gauravsinghbisen1@gmail.com>
2026-09-21 18:02:10 +0530

    straight and reverse pyramid using stars

---
+++ patterns/combined-pyramid.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern9(int n)
+{
+    for (int i = 0; i < n; i++ )
+    {
+        for(int j = 0; j < n-i-1; j++)
+        {
+            cout<<" ";
+        }
+        for(int j = 0; j < 2*i+1; j++)
+        {
+            cout<<"*";
+        }
+        for(int j = 0; j < n-i-1; j++)
+        {
+            cout<<" ";
+        }
+       cout<<endl;
+    }
+}
+void pattern10(int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            cout<<" ";
+
+        }
+        for(int j = 0; j < 2*n - (2*i+1); j++)
+        {
+            cout<<"*";
+        }
+        for(int j = 0; j < i; j++)
+        {
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    int n ;
+    cin>>n;
+    pattern9(n);
+    pattern10(n);
+}
